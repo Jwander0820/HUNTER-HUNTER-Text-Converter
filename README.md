f513d14fdc6e4d526f8faeebe9404bcc170097a7
Jwander <jasper870820@gmail.com>
2023-03-19 03:41:13 +0800

    docs: 更新README、main新增實際使用範例
    1. 更新README，附上實際使用效果
    2. main.py新增實際使用範例

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# 獵人文字轉換器
+# Hunter x Hunter Text Converter
+### 獵人文字轉換器
+### ハンター×ハンター テキストコンバーター
 
-一個簡易的獵人文字轉換器/生成器
+![Hunter x Hunter](https://github.com/Jwander0820/HUNTER-HUNTER-Text-Converter/blob/master/data/HunterxHunter.png)
+
+![Text Converter](https://github.com/Jwander0820/HUNTER-HUNTER-Text-Converter/blob/master/data/TextConverter.png)
+
+
+*一個簡易的獵人文字轉換器/生成器*
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from PIL import Image
 
 if __name__ == '__main__':
     # 使用範例
-    texts = ['ka', 'wa', 'i', "ne"]
+    texts = ['ha', 'n', "-", "ta", 'ha', 'n', "-", "ta"]  # Hunter x Hunter/ハンター×ハンター
+    texts = ['te', 'ki', "su", "to", 'ko', 'n', 'ba', "ta"]  # 文字轉換器/Text Converter/テキストコンバーター
+    texts = ['ka', 'wa', 'i', 'ne', '-']  # 可愛/cute/かわいい
 
     direction = 'horizontal'  # 'horizontal' 或 'vertical'
     result_image = GenerateText().concatenate_images(texts, direction, padding=25)
